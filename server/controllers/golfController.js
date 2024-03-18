2d13a903c926f67590964bbee26735c367737dea
unknown <Jakethetyper.jacobjennings7991@gmail.com>
2024-03-17 21:11:21 -0500

    2024

--- server/controllers/golfController.js
+++ server/controllers/golfController.js
@@ -1,19 +1,15 @@
+exports.homepage = async (req, res) => {
+  res.render("index");
+};
 
+exports.draft = async (req, res) => {
+  res.render("draft");
+};
 
+exports.history = async (req, res) => {
+  res.render("history");
+};
 
-
-
-
-
-exports.homepage = async(req, res) => {
-
-    res.render('index');
-}
-
-exports.draft = async(req, res) => {
-    res.render('draft');
-}
-
-exports.history = async(req, res) => {
-    res.render('history');
-}
+exports.tournament = async (req, res) => {
+  res.render("tournament");
+};
--- server/routes/golfLinks.js
+++ server/routes/golfLinks.js
@@ -1,10 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const golfController = require('../controllers/golfController');
+const golfController = require("../controllers/golfController");
 
-router.get('/', golfController.homepage);
-router.get('/draft', golfController.draft);
-router.get('/history', golfController.history);
-
+router.get("/", golfController.homepage);
+router.get("/draft", golfController.draft);
+router.get("/history", golfController.history);
+router.get("/tournament", golfController.tournament);
 
 module.exports = router;
